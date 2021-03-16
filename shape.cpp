49ccc628e26d4c12b1c1a878b9403c0083411c55
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-03-16 19:39:37 +0100

    Add example shape

--- shape.cpp
+++ shape.cpp
@@ -1,11 +1,72 @@
-#include <math.h>
+#include "shape.h"
 
 #include <iostream>
 
 using namespace std;
 
+void Screen::print(string message)
+{
+    cout << "\033[1;32m" << message << "\033[0m" << endl;
+}
+
+// origin(1,2)
+// origin(Point(1,2))
+Shape::Shape(int nbrSide, string name, Color color) : nbrSide(nbrSide), origin(1, 2)
+{
+    // on préfère la liste
+    // this->nbrSide = nbrSide;
+    this->name = name;
+    area = 0.0;
+}
+
+void Shape::show(){
+    screen->print( this->toString() );
+}
+
+void Shape::calculArea(){
+
+}
+
+string Shape::toString(){
+    return "Shape area : " + std::to_string(this->area) + " / " + origin.toString();
+}
+
+// class square
+Square::Square(string name, int sideLength) : Shape(4, name, Color::Red), sideLength(sideLength)
+{
+    // liste init
+    // this->sideLength = sideLength;
+}
+
+void Square::calculArea()
+{
+    area = sideLength * sideLength;
+}
+
+
+void Hexagon::calculArea()
+{
+    
+}
+
 
 void forme()
 {
+    Screen screen;
 
+    Square square("carre", 4);
+    square.setScreen(&screen);
+
+    square.calculArea();
+    square.show();
+
+    Hexagon hex("hex", 6);
+
+    Shape* shape1 = &square;
+    Shape* shape2 = &hex;
+
+    shape1->calculArea();
+
+    square.calculArea();
+    hex.calculArea();
 }
--- shape.h
+++ shape.h
@@ -1,5 +1,101 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+void forme();
+
+// entière
+enum Color
+{
+    Black,
+    Red
+};
+
+class Screen
+{
+  public:
+    void print(string message);
+};
+
+// tout est public
+struct Point
+{
+    Point(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    int x;
+    int y;
+
+    // convert Point to String
+    string toString()
+    {
+        return "X=" + std::to_string(x) + ", Y=" + to_string(y);
+    }
+};
+
 // Class shape
+class Shape
+{
+  public:
+    // Oblige de définir les valeurs
+    Shape(int nbrSide, string name, Color color = Color::Black);
+
+    int getSideNumber()
+    {
+        return nbrSide;
+    }
+
+    void setScreen(Screen* s){
+        screen = s;
+    }
+
+    void show();
+
+    string toString();
+
+    void calculArea();
+
+  protected:
+    int nbrSide;
+    string name;
+    Color color;
+    double area;
+    // toujours init pointeur
+    // init pour toute les classes
+    Screen* screen = nullptr; // agrégation
+
+    private:
+        // composition
+        Point origin;
+};
 
 // Class Square
+class Square : public Shape
+{
+  public:
+    // pas de contructeur par défaut, on oblige paramètre
+    Square(string name, int sideLength);
+
+    void calculArea();
+
+  private:
+    int sideLength;
+};
 
 // Class Hexagon
+class Hexagon : public Shape
+{
+  public:
+    // pas de contructeur par défaut, on oblige paramètre
+    Hexagon(string name, int sideLength);
+
+    void calculArea();
+
+  private:
+    int sideLength;
+};
