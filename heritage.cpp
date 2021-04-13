56ac5cc0a9ac80fb635680ec68ee4395b5e1235e
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-04-13 18:04:51 +0200

    Add heritage multiple

--- heritage.cpp
+++ heritage.cpp
@@ -4,28 +4,33 @@
 
 using namespace std;
 
-struct GrandMere 
+struct GrandMere
 {
     GrandMere(){ cout << "Grand-mère construite" << endl; }
+    GrandMere(int a) : value(a) { cout << "Grand-mère construite avec val " << a << endl; }
     int value;
 };
 
 struct Pere : public virtual GrandMere{
-    Pere(){ cout << "Pere construit" << endl; }
+    Pere(){ cout << "Père construit" << endl; }
+    Pere(int a) : GrandMere(a) { cout << "Père construit avec val" << endl; }
 };
 
 struct Mere : public virtual GrandMere{
-    Mere(){ cout << "Mere construite" << endl; }
+    Mere(){ cout << "Mère construite" << endl; }
+    Mere(int a) : GrandMere(a) { cout << "Mère construite avec val" << endl; }
 };
 
-struct  Enfant : public Mere, public Pere
+struct Enfant : public Mere, public Pere
 {
     Enfant(){ cout << "Enfant construit" << endl; }
+    Enfant(int a) : GrandMere(a), Mere(8) { cout << "Enfant construit avec val" << endl; }
 };
+
 
 void heritage()
 {
-    cout << "Example heritage" << endl;
-    Enfant e;
+    cout << "Example héritage" << endl;
+    Enfant e(12);
     cout << "Value = " << e.value << endl;
 }
