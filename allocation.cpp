d38bb4586f275c36ee2ad12474cd65910376816f
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-04-20 20:54:33 +0200

    Add exception and allocation

--- allocation.cpp
+++ allocation.cpp
@@ -7,6 +7,8 @@ using namespace std;
 class Foo
 {
   public:
+    int getValue() const { return value; }
+
     int value;
 };
 
@@ -16,6 +18,33 @@ class Bar
     double value;
 };
 
+struct Data
+{
+  int nbr;
+  int capacite;
+  Foo** tab;
+
+  // add
+  // delete
+  // init
+};
+
+Foo** alloc(int capa){
+    Foo** tab = new Foo*[capa](); // tableau de pointeurs
+
+    for( int i=0; i<capa; i++){
+      tab[i] = nullptr;
+    }
+
+    tab[0] = new Foo();
+    tab[0]->value = 12;
+
+    // tab[0] -> type -> Foo*
+    // tab    -> type -> Foo**
+
+    return tab;
+}
+
 void allocation()
 {
     // new Foo crée un objet dans la mémoire
@@ -24,6 +53,15 @@ void allocation()
     
     p2->value = 4;
     delete p;
+
+    int* tab_i = new int[10];
+    int value = *tab_i;
+    delete[] tab_i;
+
+    Foo** tab = alloc(10);
+    cout << "La valeur allocation : " << tab[0]->value << endl;
+    delete tab[0]; // supprime objet foo
+    delete[] tab; // supprime le tableau
 
     // Comme la zone mémoire ci-dessus a été libérée, elle sera
     Bar* px = new Bar();
--- exception.cpp
+++ exception.cpp
@@ -1,15 +1,44 @@
 #include <math.h>
 
 #include <iostream>
+#include <exception>
 
 using namespace std;
 
 const int ERROR_FLOATING_POINT = 100;
 
+
+class MyException : public exception
+{
+  public:
+    MyException(string const& phrase = "", int num = 0) throw() : 
+        num(num), phrase(phrase) {}
+
+    virtual const char* what() const throw()
+    {
+        return phrase.c_str();
+    }
+
+    // méthode virtuelle donc destructeur virtuel
+    virtual ~MyException() throw() {}
+
+    int num;
+  private:
+    // on peut ajouter un numéro ou priorité...    
+    int priorite;
+    string phrase;
+};
+
+
 int func2(int value){
     if( value == 0 )
-        return ERROR_FLOATING_POINT;
-    
+    {
+        //throw ERROR_FLOATING_POINT;
+        //throw "BOOM";
+        //return ERROR_FLOATING_POINT;
+        throw MyException("Problème BOOM", ERROR_FLOATING_POINT);
+    }
+   
     int calc = 14 / value;
     cout << "Le résultat est : " << calc << endl;
 
@@ -18,16 +47,32 @@ int func2(int value){
 
 int func1(int value){
 
-    int ret = func2(value);
-    if( ret != 0 ){
-        return ret;
+    int ret;
+    int val;
+    try
+    {
+        // visibilité
+        val = 12;
+        ret = func2(value);
+
+        // code 1
     }
+    catch(MyException& e)
+    {
+        val = 1;
+        cout << "Exc func1 " << endl;
+        throw; // propage l'exception
+    }
+    
+    //if( ret != 0 ){
+    //    return ret;
+    //}
     
     int tmp = value * 4;
     ret = func2(tmp);
-    if( ret != 0 ){
-        return ret;
-    }
+    //if( ret != 0 ){
+    //    return ret;
+    //}
 
     return 0;
 }
@@ -36,10 +81,32 @@ void exceptions()
 {
     cout << "\033[1;32m" << "Exemple sur les exceptions" << "\033[0m" << endl;
 
-    int ret = func1(0);
-    if( ret == ERROR_FLOATING_POINT ){
-        cout << "BOOM" << endl;
+    try
+    {
+        int ret = func1(0);
     }
+    catch(double value)
+    {
+       cout << "Il y a eu un problème double : " << value << endl; 
+    }
+    catch(int var)
+    {
+       cout << "Il y a eu un problème entier : " << var << endl; 
+    }
+    catch(const char* txt){
+        cout << "Il y a eu un problème : " << txt << endl;    
+    }
+    catch(MyException& e){
+        cout << "Il y a eu un problème : " << e.what() << " le num : "  << e.num << endl;    
+    }
+    catch(...)
+    {
+       cout << "Il y a eu un problème" << endl; 
+    }
+    
+    //if( ret == ERROR_FLOATING_POINT ){
+    //    cout << "BOOM" << endl;
+    //}
 
     cout << "Fin de l'exemple pour les excéptions" << endl;
     
