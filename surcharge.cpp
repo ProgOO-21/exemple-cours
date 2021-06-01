3f59d48821650303e10f4be1d11fca4a4fe2d1a7
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-06-01 17:58:43 +0200

    Surcharge in progress

--- surcharge.cpp
+++ surcharge.cpp
@@ -6,6 +6,11 @@ class Vector{
 
 };
 
+Vector operator+(const Vector& p1, const Vector& p2){
+    return Vector();
+}
+
+
 class Point
 {
   public:
@@ -22,6 +27,20 @@ class Point
         return y;
     }
 
+    void add(const Point& p1)
+    {
+        this->x += p1.x;
+        this->y += p1.y;
+    }
+
+    Point& operator+=(const Point& p)
+    {
+        this->add(p);
+        return *this;
+    }
+
+    friend bool operator==(const Point& p1, const Point& p2);
+
   private:
     int x;
     int y;
@@ -34,14 +53,20 @@ Point add_point(const Point& p1, const Point& p2){
     return p;
 }
 
-
-Vector operator+(const Vector& p1, const Vector& p2){
-    return Vector();
-}
-
 Point operator+(const Point& p1, const Point& p2)
 {
     return add_point(p1, p2);
+}
+
+bool operator==(const Point& p1, const Point& p2)
+{
+    return p1.x == p2.x && p1.y == p2.y; // a cause de friend
+    //return p1.getX() == p2.getX() && p1.getY() == p2.getY();
+}
+
+ostream& operator<<(ostream& out, const Point& p)
+{
+    return out << "x : " << p.getX() << " / y : " << p.getY();
 }
 
 void surcharge()
@@ -52,6 +77,15 @@ void surcharge()
     Point p2(3, 1);
 
     Point p = p1 + p2;
+
+    cout << "Test add p " << endl;
+    Point pp(1,2);
+    pp += Point(3,4);
+    cout << pp << endl;
+
+    if( p == Point(15,5) ){
+        cout << "Les points sont égaux" << endl;
+    }
 
     int val1 = 12;
     val1 += 3;
